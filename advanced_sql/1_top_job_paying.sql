2d605e639868e1d8c7a59d23dbe23a4d52f6f5c9
Nimish Tiwari <ajaynimishr3p@gmail.com>
2025-03-03 18:04:55 +0530

    top_paying_jobs

--- advanced_sql/1_top_job_paying.sql
+++ advanced_sql/1_top_job_paying.sql
@@ -1,5 +1,11 @@
-SELECT *
-FROM job_postings_fact
+SELECT jpf.job_id,
+    cd.name AS company_name,
+    jpf.job_title,
+    jpf.job_title_short,
+    jpf.job_location,
+    jpf.salary_year_avg
+FROM job_postings_fact AS jpf
+LEFT JOIN company_dim AS cd ON jpf.company_id = cd.company_id
 WHERE job_title_short = 'Business Analyst' AND salary_year_avg IS NOT NULL AND job_location = 'Anywhere'
 ORDER BY salary_year_avg DESC
 LIMIT 10;
